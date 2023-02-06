5a92f4929560b590d36135f9078b5a47bde4b26d
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2023-02-06 17:00:19 +0200

    login

--- src/components/auth/pages/Login.js
+++ src/components/auth/pages/Login.js
@@ -1,21 +1,85 @@
 import React from 'react'
 import { Form, Formik } from 'formik';
+import {Avatar, Box, Button, Checkbox, FormControlLabel, Grid, Link, TextField, Typography  } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
-
-
-const initialValues = { username: '', password: '',  };
+// const initialValues = { username: '', password: '',  };
 
 function Login() {
-    
+      
     return (
-        <Formik
-        initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={loginValidationSchema}>
+      <div className='login'>
+                <Formik>
             <Form>
+            <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+          >
+             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                  <Link
+                    href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
 
+
+
+
+             </Box>
             </Form>
    </Formik>
+</div>
                
   )
 }
